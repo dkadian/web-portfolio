69b307e29bdcaf1bbde4d02724ef223fca6e76e8
dkadian <deepakkadian581@gmail.com>
2025-02-05 13:53:40 +0530

    u1

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page = () => {
   return (
     <div className='h-screen justify-center items-center flex text-2xl'>
-      <h1 className='border border-neutral-800 p-10 rounded-xl hover:scale-110'>Deepak</h1>
+      <h1 className='border border-neutral-800 p-10 rounded-xl hover:bg-white scale-110'>Deepak</h1>
     </div>
   );
 }
