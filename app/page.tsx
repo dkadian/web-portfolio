b4828f03aba236f32b9822209ff592070f2fb5ec
dkadian <deepakkadian581@gmail.com>
2025-02-05 13:35:15 +0530

    v1

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div className='h-screen justify-center items-center flex text-2xl'>
+      <h1 className='border border-neutral-800 p-10 rounded-xl hover:scale-110'>Deepak Kadian</h1>
+    </div>
+  );
+}
+
+export default Page;
